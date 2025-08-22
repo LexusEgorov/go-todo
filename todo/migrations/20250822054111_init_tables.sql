5742673320acb7d9b586744ac8c9618b8787835f
lexusegorov <alexey.egorov.job@gmail.com>
2025-08-22 16:13:36 +0700

    feat: add migrations

---
+++ todo/migrations/20250822054111_init_tables.sql
@@ -0,0 +1,31 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    tg_id BIGINT NOT NULL,
+    name TEXT,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    u_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    text TEXT,
+    status TEXT NOT NULL,
+    deadline TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    CONSTRAINT fk_tasks_users FOREIGN KEY (uid) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_tasks_uid ON tasks (uid);
+CREATE INDEX idx_users_tg_id ON users (tg_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+-- +goose StatementEnd
